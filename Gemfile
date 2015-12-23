c582c95baee97359588819f38ddb16709e33b7ff
Justin Langhorst <the@justinlanghorst.com>
2015-12-23 00:17:26 -0600

    Set-up a testing framework starting with a test for msgbox; remove shoulda requirement

--- Gemfile
+++ Gemfile
@@ -6,9 +6,11 @@ source "http://rubygems.org"
 # Add dependencies to develop your gem here.
 # Include everything needed to run rake, tests, features, etc.
 group :development do
-  gem "shoulda", ">= 0"
-  gem "rdoc", "~> 3.12"
-  gem "bundler", "~> 1.0"
-  gem "jeweler", "~> 2.0.1"
-  gem "simplecov", ">= 0"
+  gem 'rdoc', '~> 3.12'
+  gem 'bundler', '~> 1.0'
+  gem 'jeweler', '~> 2.0.1'
+  gem 'simplecov', '>= 0'
+  gem 'minitest', '~> 5.8'
+  gem 'minitest-reporters', '~> 1.1'
 end
+  
--- test/test_mrdialog.rb
+++ test/test_mrdialog.rb
@@ -1,7 +1,48 @@
-require 'helper'
+require 'minitest/autorun'
+require 'minitest/reporters'
+require_relative '../lib/mrdialog'
 
-class TestMrdialog < Test::Unit::TestCase
-  should "probably rename this file and start testing for real" do
-    flunk "hey buddy, you should probably rename this file and start testing for real"
+Minitest::Reporters.use!
+
+class TestMRDialog < Minitest::Test
+
+  attr_reader :dialog
+
+  def setup
+    @dialog = MRDialog.new
+    @dialog.dry_run = true
+  end
+
+  def commands
+    { msgbox: [ 'Test text', 24, 80 ],
+    }
+  end
+
+  #
+  # Commands
+  #
+
+  def test_msgbox_command
+    dialog.msgbox('Test text', 24, 80)
+    assert_includes(dialog.last_cmd, 'Test text')
+    assert_includes(dialog.last_cmd, ' 24 80')
+  end
+
+  #
+  # Options 
+  # 
+  def test_ascii_lines_option
+    dialog.ascii_lines = false
+
+    commands.each do |method, arguments|
+      dialog.send(method, *arguments)
+      refute_includes(dialog.last_cmd, '--ascii-lines')
+    end
+
+    dialog.ascii_lines = true
+    commands.each do |method, arguments|
+      dialog.send(method, *arguments)
+      assert_includes(dialog.last_cmd, '--ascii-lines')
+    end
   end
 end
